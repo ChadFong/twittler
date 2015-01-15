3224ae0ed3bef3d3b5f9694dd85cbbdd85d116b4
ChadFong <ChadMFong@Gmail.com>
2015-01-15 15:21:28 -0800

    Reworked how tweets are created into the makeTweet function.  Can be called without arguments to update the main tweet feed, or called with a user's name to show that user's tweets.

--- twittler.js
+++ twittler.js
@@ -2,62 +2,49 @@ $(document).ready(function(){
 var $main = $('main');
 $main.html('<p>All Tweets</p>');
 
-var index = streams.home.length - 1;
-while(index >= 0){
-  var tweet = streams.home[index];
-  var $tweet = $('<div></div>');
-  $tweet.text('@' + tweet.user + ': ' + tweet.message);
-  $tweet.attr('id', tweet.user);
-  /*
-  var time = timeCal(tweet.created_at);
-  var $time = $('<p class="time"></p>');
-  $time.text(time);
-  $tweet.append($time);
-  */
-  $tweet.appendTo($main);
-  index -= 1;
-}
 
-var tweetReader = function(user) {
-	$(".userTweets").empty();
-	tweetNum = user.length - 1;
-	$('.userTweets').append($("<p>" + user[0].user + "'s Tweets</p>"));
-	while(tweetNum >= 0) {
-		var tweet = user[tweetNum];
+var makeTweet = function(user){
+	if(user !== undefined) {
+		index = user.length-1;
+		var tweet = function(){return user[index];};
+		var location = $('.userTweets');
+	}
+	else {
+		index = streams.home.length-1;
+		var tweet = function(){return streams.home[index];};
+		var location = $main;
+	}
+
+	while(index >= 0) {
 		var $tweet = $('<div></div>');
+		$tweet.text('@' + tweet().user + ': ' + tweet().message);
+  		$tweet.attr('id', tweet().user);
 
-
-		$tweet.text('@' + tweet.user + ': ' + tweet.message)
-		
-		var time = timeCal(tweet.created_at);
+  		var end = Date.now();
+		var elapsed = end - tweet().created_at;
+		var hours = Math.floor(elapsed/36e5);
+		var mins = Math.floor((elapsed % 36e5) / 6e4);
+		var secs = Math.floor((elapsed % 6e4) / 1000);
+		var string = "  Posted " + hours + ":" + mins + ":" + secs + " ago";
 		var $time = $('<p class="time"></p>');
-
-		$time.text(time);
-		
+		$time.text(string);
 		$tweet.append($time);
-		
-		$tweet.appendTo($('.userTweets'))
-		tweetNum -= 1;
+  		
+  		$tweet.appendTo(location);
+  		index -= 1;
 	}
 }
 
-var timeCal = function(start) {
-	var end = Date.now();
-	var elapsed = end - start;
-	var hours = Math.floor(elapsed/36e5);
-	var mins = Math.floor((elapsed % 36e5) / 6e4);
-	var secs = Math.floor((elapsed % 6e4) / 1000);
-	var string = "  Posted " + hours + ":" + mins + ":" + secs + " ago";
-	return string;
-}
-
+makeTweet();
 
 var test = $('<p>It worked!</p>');
 
 $('div').click(function() {
 	var name = $(this).attr("id");
 	var userTweets = streams.users[name];
-	tweetReader(userTweets);
+	$(".userTweets").empty();
+	$('.userTweets').append($("<p>" + name + "'s Tweets</p>"));
+	makeTweet(userTweets);
 });
 
 $(":button").click(function(){
